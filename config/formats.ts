faf9f4a7a595758aa120ff153bfc12366a779cb2
Bonome <62403410+Bonome777@users.noreply.github.com>
2025-01-21 00:53:46 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -26,13 +26,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		section: "S/V Singles",
 	},
 	{
-		name: "[Gen 9] Random Battle",
-		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
-		mod: 'gen9',
-		team: 'random',
-		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
-	},
-	{
 		name: "[Gen 9] Unrated Random Battle",
 		mod: 'gen9',
 		team: 'random',
